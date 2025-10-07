afb0d52dcaf3dd31ed2b75cd110aa6751be3d839
will <morrisonwill0101@gmail.com>
2025-10-06 22:27:59 -0700

    Change: fix finder files not being found

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 62ae66deb0eb91f4304e0a078da0ef1854727826
+AESD_ASSIGNMENTS_VERSION = 9cd120535c4f3d8db599900b4b55f61db82ebf68
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,9 +23,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/bin
+#	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
